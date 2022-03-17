e689906974ec0fcc2c5021a40508d326a3b9407e
Alex L <12oprs@gmail.com>
2022-03-17 22:57:55 +1200

    prepared for deploy

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,7 +4,10 @@ import io.javalin.Javalin;
 
 public class App {
 
-    private static Javalin app;
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.valueOf(port);
+    }
 
     public static Javalin getApp() {
         Javalin app = Javalin.create();
@@ -12,9 +15,9 @@ public class App {
         return app;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(5000);
+        app.start(getPort());
         app.get("/", ctx -> ctx.result("Hello, world!"));
     }
 
