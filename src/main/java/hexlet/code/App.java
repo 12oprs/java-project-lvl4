46163d345e8262e622a1e03ebb2d57c7900ed8e5
Alex L <12oprs@gmail.com>
2022-03-21 00:32:13 +1200

    step 4

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,6 +1,12 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 public class App {
 
@@ -9,16 +15,45 @@ public class App {
         return Integer.valueOf(port);
     }
 
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
     public static Javalin getApp() {
-        Javalin app = Javalin.create();
-        app.create(config -> config.enableDevLogging());
+        Javalin app = Javalin.create(config -> {
+            if (!isProduction()) {
+                config.enableDevLogging();
+            }
+            JavalinThymeleaf.configure(getTemplateEngine());
+            config.enableWebjars();
+        });
+
         return app;
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+        return templateEngine;
     }
 
     public static void main(String[] args) {
         Javalin app = getApp();
         app.start(getPort());
-        app.get("/", ctx -> ctx.result("Hello, world!"));
+        app.get("/", ctx -> ctx.render("index.html"));
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
     }
 
 }
